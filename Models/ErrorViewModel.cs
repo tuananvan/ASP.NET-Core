33821a4cb36b1f0fe1ef2fbf9f4e61de8cbd938e
ishopjapan <minhtuandang127@gmail.com>
2023-08-24 21:02:46 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace demo_ASP.NET_Core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+<script>
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+        signUpButton.addEventListener('click', () => {
+        container.classList.add('right-panel-active');
+        });
+
+        signInButton.addEventListener('click', () => {
+        container.classList.remove('right-panel-active');
+        });
+</script>
